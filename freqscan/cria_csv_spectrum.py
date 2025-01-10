407d7f52c781573f4c3dca16b837a229ed25a0f4
Alfa Marine <alfamarine.ferreira@gmail.com>
2025-01-10 19:56:59 -0300

    atualiza faixa espectral

--- freqscan/cria_csv_spectrum.py
+++ freqscan/cria_csv_spectrum.py
@@ -3,10 +3,10 @@ import numpy as np
 
 frequencia_inicial = 60
 frequencia_final = 3000
-passo_frequencia = 0.1
+passo_frequencia = 15
 
 harmonico_pd = pd.DataFrame()
-harmonico_pd['harmonico'] = np.arange(frequencia_inicial,(frequencia_final + passo_frequencia),passo_frequencia).tolist()
+harmonico_pd['harmonico'] = np.arange(frequencia_inicial,(frequencia_final + 1),passo_frequencia).tolist()
 harmonico_pd['harmonico'] = harmonico_pd['harmonico'].apply(lambda h : round((h/60), 6))
 harmonico_pd['amplitude'] = 100
 harmonico_pd['angulo'] = 0
